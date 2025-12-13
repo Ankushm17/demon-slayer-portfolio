ee58564ed4b4c4cfc0267d7206080dfa59552789
ankush <ankushmadan17@gmail.com>
2025-12-13 14:59:32 +0530

    Configured Github pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Runner from './components/Runner';
 import Home from './pages/Home';
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="app-container">
         
         {/* These stay on screen permanently */}
@@ -23,7 +23,7 @@ function App() {
 
         
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
